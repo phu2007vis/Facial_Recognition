d221c45db096680e58bd8f61fd4ec5ed6dd3df7b
phuoc <21013187@st.phenikaa-uni.edu.vn>
2024-04-04 22:17:44 +0700

    pass

--- demo.py
+++ demo.py
@@ -23,13 +23,16 @@ def main():
             if not ret:
                 break
             with torch.no_grad():
-                ages,gioi_tinhs = predictor.recognize(img)
-                if  len(ages):
-                    age = ages[0]
-                    put_text(img,f"Age : {age}")
-                if len(gioi_tinhs):
-                    gioi_tinh = gioi_tinhs[0]
-                    put_text(img,f"Sex : {gioi_tinh}",(50))
+                try:
+                    ages,gioi_tinhs = predictor.recognize(img)
+                    if  len(ages):
+                        age = ages[0]
+                        put_text(img,f"Age : {age}")
+                    if len(gioi_tinhs):
+                        gioi_tinh = gioi_tinhs[0]
+                        put_text(img,f"Sex : {gioi_tinh}",(50))
+                except:
+                    pass
             cv2.imshow("image",img)
             if cv2.waitKey(8) == ord("q"):
                 break
