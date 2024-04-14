3f3c2c28ae0163448e32c621355fbac2ea22368f
phuoc <21013187@st.phenikaa-uni.edu.vn>
2024-04-14 15:16:16 +0700

    add some code to prevent bug in partition file

--- resources/folder_utils/partition_face_by_person.py
+++ resources/folder_utils/partition_face_by_person.py
@@ -6,18 +6,21 @@ from tqdm import tqdm
 from multiprocessing import Pool
 
 def copy_file(args):
-    root_folder, filename, label, outfolder = args
+    root_folder, filename, label, outfolder, skip_empty = args
     destination_folder = os.path.join(outfolder, str(label))
     os.makedirs(destination_folder, exist_ok=True)
     file_path = os.path.join(root_folder, filename)
+    if not os.path.exists(file_path):
+        if skip_empty:
+            return
     output_file_path = os.path.join(destination_folder, filename)
     shutil.copy(file_path, output_file_path)
 
-def partition_folder(annotation_file=r"D:\download\identity_CelebA.txt", root_folder=r"D:\download\face_celeba\img_align_celeba", outfolder=r"D:\download\DataBasePartitioned"):
+def partition_folder(annotation_file=r"D:\download\identity_CelebA.txt", root_folder=r"D:\download\face_celeba\img_align_celeba", outfolder=r"D:\download\DataBasePartitioned",skip_empty = False):
     data = pd.read_csv(annotation_file, header=None, sep=" ")
     filename2id = {file_name: label for file_name, label in zip(data[0], data[1])}
 
-    args_list = [(root_folder, filename, label, outfolder) for filename, label in filename2id.items()]
+    args_list = [(root_folder, filename, label, outfolder,skip_empty) for filename, label in filename2id.items()]
 
     with Pool() as pool:
         list(tqdm(pool.imap(copy_file, args_list), total=len(args_list), desc="Processing"))
