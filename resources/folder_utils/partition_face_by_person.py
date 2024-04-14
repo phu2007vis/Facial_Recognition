4e40320d10a0283bf57c29846d5cd47cc3fe9c6b
phuoc <21013187@st.phenikaa-uni.edu.vn>
2024-04-14 15:11:35 +0700

    add parallel id partition

--- resources/folder_utils/partition_face_by_person.py
+++ resources/folder_utils/partition_face_by_person.py
@@ -1,26 +1,28 @@
 
-import os.path as osp
 import os
 import shutil
 import pandas as pd
 from tqdm import tqdm
+from multiprocessing import Pool
 
-def parition_folder(annotation_file = r"D:\download\identity_CelebA.txt",root_folder = r"D:\download\face_celeba\img_align_celeba",outfolder = r"D:\download\DataBasePartitioned"):
-    proces_bar  = tqdm(os.listdir(root_folder))
-    data = pd.read_csv(annotation_file,header=None,sep=" ")
-    filename2id = {}
-    for i in range(data.shape[0]):
-        file_name,label = data.iloc[i]
-        filename2id[file_name] = label
+def copy_file(args):
+    root_folder, filename, label, outfolder = args
+    destination_folder = os.path.join(outfolder, str(label))
+    os.makedirs(destination_folder, exist_ok=True)
+    file_path = os.path.join(root_folder, filename)
+    output_file_path = os.path.join(destination_folder, filename)
+    shutil.copy(file_path, output_file_path)
 
-    for filename in filename2id.keys():
-        proces_bar.update(1)
-        destination_folder = osp.join(outfolder,str(filename2id[filename]))
-        os.makedirs(destination_folder,exist_ok=True)
-        file_path = osp.join(root_folder,filename)
-        output_file_path = osp.join(destination_folder,filename)
-        shutil.copy(file_path,output_file_path)
+def partition_folder(annotation_file=r"D:\download\identity_CelebA.txt", root_folder=r"D:\download\face_celeba\img_align_celeba", outfolder=r"D:\download\DataBasePartitioned"):
+    data = pd.read_csv(annotation_file, header=None, sep=" ")
+    filename2id = {file_name: label for file_name, label in zip(data[0], data[1])}
+
+    args_list = [(root_folder, filename, label, outfolder) for filename, label in filename2id.items()]
+
+    with Pool() as pool:
+        list(tqdm(pool.imap(copy_file, args_list), total=len(args_list), desc="Processing"))
+
 
         
 if __name__ == "__main__":
-    parition_folder()
+    partition_folder()
